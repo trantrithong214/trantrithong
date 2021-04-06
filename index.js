b48751b1eee954ad79203c490261766733e6706b
trantrithong214 <trantrithong2141997@gmail.com>
2021-04-06 13:57:39 +0700

    a

--- index.js
+++ index.js
@@ -41,41 +41,6 @@ io.on("connection", (socket) => {
     } else console.log("user ", socket.id, " disconnected");
   });
 
-  socket.on("create-station", (station) => {
-    let stationTemp = station;
-    stationTemp.id = socket.id;
-    console.log("new station info: ", stationTemp);
-    stations.push(stationTemp);
-    stations_ID.push(stationTemp.id);
-    console.log("stations_ID[]: ", stations_ID);
-
-    io.to(socket.id).emit("station-id", socket.id);
-    io.emit("stations", stations);
-  });
-
-  socket.on("update-station", (station) => {
-    console.log("station will updated: ", station.name, ": ", station.id);
-    let isStationUpdate = (element) => element.id == station.id;
-    let index = stations.findIndex(isStationUpdate);
-    stations[index] = station;
-    console.log("updated station info: ", stations[index]);
-    io.emit("stations", stations);
-  });
-
-  socket.on("list-rooms", (msg) => {
-    console.log(stations);
-    io.emit("stations", stations);
-  });
-
-  socket.on("join-room", (msg) => {
-    socket.leaveAll();
-    socket.join(msg);
-
-    console.log("join-room", msg);
-
-    console.log("client in station: ", socket.adapter.rooms);
-  });
-
   socket.on("temp", (msg) => {
     io.to(socket.id).emit("temp2web", msg);
   });
