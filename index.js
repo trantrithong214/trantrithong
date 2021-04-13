ba5a08e6884349ff12be45f2cbae04b7296d61a0
trantrithong214 <trantrithong2141997@gmail.com>
2021-04-13 14:15:28 +0700

    a

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("temp", (msg) => {
-    msgg = Object.assign(msg);
-    io.emit("temp2web", msgg);
+    io.emit("temp2web", msg);
   });
 });
